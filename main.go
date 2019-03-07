2cbf341a409a57e1e771a6860911186c48109038
Kazuki Nakamura <kazuki.nakamura@tcmobile.jp>
2019-03-07 17:53:41 +0900

    fix

--- main.go
+++ main.go
@@ -61,7 +61,7 @@ func main() {
 		log.Fatal("$PORT must be set")
 	}
 
-	allowedOrigins := handlers.AllowedOrigins([]string{"http://localhost:8080/"})
+	allowedOrigins := handlers.AllowedOrigins([]string{"http://localhost:8080"})
 	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "DELETE", "PUT"})
 	allowedHeaders := handlers.AllowedHeaders([]string{"Authorization"})
 
